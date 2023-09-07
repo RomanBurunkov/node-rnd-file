4e770d90f4e6a778b07a86ae5cfddc5a08c432e6
RomanBurunkov <romon2002@gmail.com>
2023-09-07 18:34:06 +0300

    Fix tests

--- test/rnd.file.spec.js
+++ test/rnd.file.spec.js
@@ -2,9 +2,22 @@ const { join } = require('node:path');
 const { rm, stat } = require('node:fs/promises');
 const { makeDir, generate, randomFile } = require('../lib/rnd.file');
 
-describe('randomFile', () => {
+describe('makeDir', () => {
   test('should be a function', () => {
-    expect(typeof randomFile).toBe('function');
+    expect(typeof makeDir).toBe('function');
+  });
+
+  test('should create a directory', async () => {
+    const path = join(__dirname, 'tmp', 'test');
+    await rm(path, { recursive: true, force: true });
+    const created = await makeDir(path);
+    expect(created).toBe(true);
+  });
+
+  test('should return false if directory already exist ', async () => {
+    const path = join(__dirname, 'tmp', 'test');
+    const created = await makeDir(path);
+    expect(created).toBe(false);
   });
 });
 
@@ -14,40 +27,25 @@ describe('generate', () => {
   });
 
   test('should generate a file', async () => {
-    const path = join(__dirname, 'tmp', 'random.file');
+    const dir = join(__dirname, 'tmp');
+    const path = join(dir, 'random.file');
     const size = 2000;
     const chunk = 1024;
+    await makeDir(dir);
     await generate(path, size, chunk);
     const stats = await stat(path);
     expect(stats.size).toBe(size);
   });
 
   test('should call callback on each chunk', async () => {
-    const path = join(__dirname, 'tmp', 'random.file');
+    const dir = join(__dirname, 'tmp');
+    const path = join(dir, 'random.file');
     const size = 2000;
     const chunk = 1024;
     const cb = jest.fn();
+    await makeDir(dir);
     await generate(path, size, chunk, cb);
     expect(cb).toHaveBeenCalledTimes(2);
-  });
-});
-
-describe('makeDir', () => {
-  test('should be a function', () => {
-    expect(typeof makeDir).toBe('function');
-  });
-
-  test('should create a directory', async () => {
-    const path = join(__dirname, 'tmp', 'test');
-    await rm(path, { recursive: true });
-    const created = await makeDir(path);
-    expect(created).toBe(true);
-  });
-
-  test('should return false if directory already exist ', async () => {
-    const path = join(__dirname, 'tmp', 'test');
-    const created = await makeDir(path);
-    expect(created).toBe(false);
   });
 });
 
